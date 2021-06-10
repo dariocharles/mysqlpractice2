54eb5746f6bc6067eac068ac1fad840e9aae42c1
Dario Charles <dariocharles@outlook.com>
2021-06-10 15:00:08 +0000

    Praticing crud functions of MongoDB by building a compand like front end interface

---
+++ freecodecamp-sql.py
@@ -0,0 +1,35 @@
+import mysql.connector
+from mysql.connector import Error
+import pandas as pd
+
+def create_server_connection(host_name, user_name, user_password):
+    connection = None
+    try:
+        connection = mysql.connector.connect(
+            host=host_name,
+            user=user_name,
+            passwd=user_password,
+            database=db_name
+        )
+        print("MySQL Database connection successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+    return connection
+
+    pw = "Yes" # IMPORTANT! Put your MySQL Terminal password here.
+    db = "school" # This is the name of the database we will create in the next step - call it whatever you like.
+
+    connection = create_server_connection("localhost", "root", pw)
+
+    
+def create_database(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        print("Database created successfully")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+create_database_query = "CREATE DATABASE school"
+create_database(connection, create_database_query)
